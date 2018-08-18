5c3d4d956b63d6d696d3060ee7bb2c09a6eef818
Ansel Adams <dj@volaciousbrands.com>
2018-08-18 14:50:25 -0500

    add new opportunity form and POST

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,12 +12,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get('/example/:id', function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render('example', {
-        example: dbExample
-      });
-    });
+  app.get('/add/opportunity', function(req, res) {
+    res.render('add-opportunity');
   });
 
   // Render 404 page for any unmatched routes
