ad73f893aa0719a6ae46578a9a3a2798abe191f1
Rob Phillips <robbyphillips@gmail.com>
2018-08-18 11:43:50 -0500

    create `get` routes for user, collection, item

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,4 +21,40 @@ module.exports = function (app) {
       res.json(dbExample);
     });
   });
+
+  app.get('/api/users', function (req, res) {
+    db.User.findAll({}).then(function (data) {
+      res.json(data);
+    });
+  });
+
+  app.get('/api/users/:id', function (req, res) {
+    db.User.findById(req.params.id).then( function (dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  app.get('/api/collections', function (req, res) {
+    db.Collection.findAll({}).then(function (dbCollections) {
+      res.json(dbCollections);
+    });
+  });
+
+  app.get('/api/collections/:id', function (req, res) {
+    db.Collection.findById(req.params.id).then(function (dbCollection) {
+      res.json(dbCollection);
+    });
+  });
+
+  app.get('/api/items', function (req, res) {
+    db.Item.findAll({}).then(function (dbItems) {
+      res.json(dbItems);
+    });
+  });
+
+  app.get('/api/items/:id', function (req, res) {
+    db.Item.findById(req.params.id).then(function (dbItem) {
+      res.json(dbItem);
+    });
+  });
 };
