0cea786ca2183a7dd041e0d38fff82c20bb1844b
Ansel Adams <dj@volaciousbrands.com>
2018-08-20 23:23:32 -0500

    pull data from db to handlebars, db seeds for testing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,14 @@ var db = require('../models');
 module.exports = function(app) {
   // Load index page
   app.get('/', function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render('index', {
-        msg: 'Welcome!',
-        examples: dbExamples
-      });
+    db.Opportunity.findAll({
+      include: [db.User]
+    }).then(function(dbOpportunities) {
+      var hbsObj = {
+        opportunities: dbOpportunities
+      };
+      console.log(hbsObj.opportunities);
+      res.render('index', hbsObj);
     });
   });
 
@@ -18,7 +21,14 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get('/collections', function(req, res) {
-    res.render('collections');
+    db.Collection.findAll({
+      include: [db.Item]
+    }).then(function(dbCollections) {
+      var hbsObj = {
+        collections: dbCollections
+      };
+      res.render('collections', hbsObj);
+    });
   });
 
   // Render 404 page for any unmatched routes
